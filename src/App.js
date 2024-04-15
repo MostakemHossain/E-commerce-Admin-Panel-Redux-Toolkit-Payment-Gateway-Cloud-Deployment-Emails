b5a955685fddaffcc931afa57b90b3b0a289c4be
MostakemHossain <mostakememon123@gmail.com>
2024-04-15 22:53:25 +0600

    Home section layout added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Counter } from './features/counter/Counter';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+     <Home>
+      
+     </Home>
     </div>
   );
 }
